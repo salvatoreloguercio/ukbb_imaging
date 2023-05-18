817f475ee7e24bbc326cfb5d58c65a47de06b1b9
salvatoreloguercio <salvatore.loguercio@gmail.com>
2023-05-17 18:22:39 -0700

    fix

--- convert_dicom_nifti.sh
+++ convert_dicom_nifti.sh
@@ -4,11 +4,11 @@
 
 while read batch; do
 
-  start=echo $batch | $(cut -d "_" -f 1)
-  stop=echo $batch | $(cut -d "_" -f 2)
+  start=$( echo $batch | cut -d "-" -f 1 )
+  stop=$( echo $batch | cut -d "-" -f 2 )
 
 
-  sbatch --export=start=$start,stop=$stop --job-name=convert_dicom_nifti_$batch --output=convert_dicom_nifti_$batch.%J convert_dicom_nifti.sbatch;
+  sbatch --export=start=$start,stop=$stop --job-name=convert_d_n_$batch --output=convert_d_n_$batch.%J convert_dicom_nifti.sbatch;
   sleep 1;
 
 done < batches.txt
